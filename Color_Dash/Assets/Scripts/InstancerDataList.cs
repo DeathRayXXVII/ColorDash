b66cc21760b8f516b3f530cb6720f57630abd230
Riley Anderson <10803452@uvu.edu>
2022-10-06 19:54:40 -0600

    Made a temp instancer with data and TMP

---
+++ Color_Dash/Assets/Scripts/InstancerDataList.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InstancerDataList : Instancers
+{
+    public List<Instancers> instancerDataList;
+    private int num;
+}
